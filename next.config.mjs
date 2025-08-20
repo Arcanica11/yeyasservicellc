b54fe54b52c9279ebf36b7ac8ddb90e679e5b949
Akanica <arcanicateg@gmail.com>
2025-08-20 10:15:31 -0500

    fix netx.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // This includes the locales for server-side rendering
+    outputFileTracingIncludes: {
+      '/': [
+        './src/app/i18n/locales/en/common.json',
+        './src/app/i18n/locales/es/common.json'
+      ],
+    },
+  },
 };
 
-// Cambia esta línea
 export default nextConfig;
